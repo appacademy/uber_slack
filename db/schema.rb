2affe45482def9ca37f058f262096610ffeda3db
Edmund Li <edmund.xz.lee@gmail.com>
2015-11-07 17:18:16 -0800

    migrated new authorizations table

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,18 @@ ActiveRecord::Schema.define(version: 20151108003341) do
   enable_extension "plpgsql"
 
   create_table "authorizations", force: :cascade do |t|
-    t.integer  "user_id",         null: false
-    t.string   "uber_auth_token", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.integer  "slack_user_id"
+    t.string   "slack_auth_token"
+    t.string   "oauth_session_token"
+    t.integer  "uber_user_id"
+    t.string   "uber_auth_token"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
   end
+
+  add_index "authorizations", ["oauth_session_token"], name: "index_authorizations_on_oauth_session_token", using: :btree
+  add_index "authorizations", ["slack_auth_token"], name: "index_authorizations_on_slack_auth_token", using: :btree
+  add_index "authorizations", ["slack_user_id", "uber_user_id"], name: "index_authorizations_on_slack_user_id_and_uber_user_id", unique: true, using: :btree
+  add_index "authorizations", ["uber_auth_token"], name: "index_authorizations_on_uber_auth_token", using: :btree
 
 end
