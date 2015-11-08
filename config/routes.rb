5feacbdf4499b4f17a9fee8e13831a2be4115eba
Edmund Li <edmund.xz.lee@gmail.com>
2015-11-07 17:12:19 -0800

    fixed routers confilts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-
-=======
-  namespace :api do 
-    post :echo, to: 'authorizations#echo' 
+  namespace :api do
+    post :echo, to: 'authorizations#echo'
   end
->>>>>>> 76864b4547048c4c9b608f9128f765e72e174bba
 end
