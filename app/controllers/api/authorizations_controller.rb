ddb07063781f883b7b013a516398e5dd78f5bf5f
Edmund Li <edmund.xz.lee@gmail.com>
2015-11-07 18:13:10 -0800

    added request method to uber

--- app/controllers/api/authorizations_controller.rb
+++ app/controllers/api/authorizations_controller.rb
@@ -9,4 +9,24 @@ class Api::AuthorizationsController < ApplicationController
 
   def destroy
   end
+
+  # uber
+  def update
+  	params = {
+  		client_secret: ENV['uber_client_secret'],
+  		client_id:     ENV['uber_client_id'],
+  		grant_type:    'authorization_code',
+  		# redirect_uri   ENV[''],
+  		code:          params[:code]
+  	}
+  	# post request to uber
+  	resp = Net::HTTP.post_form(URI.parse('https://login.uber.com/oauth/v2/token'), params)
+
+  	access_token = resp['access_token']
+
+  	Authorization.find_by(session_token: session[:session_token])
+  							 .update(uber_auth_token: access_token)
+
+  	render text: "uber auth success, access_token: #{access_token}"
+  end
 end
