4746af2d724bb41771b61dce2844b217d3ddd5b2
Edmund Li <edmund.xz.lee@gmail.com>
2015-11-08 10:19:26 -0800

    changed routes based on naming change

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
   namespace :api, defaults: { format: 'json' } do
-    post '/authorize', to: 'users#authorize'
-    post '/create', to: 'users#create'
+    post '/authorize', to: 'authorizations#authorize'
+    post '/connect_uber', to: 'authorizations#create'
   end
 end
