29619811a70810893e3353791ebf930e43dbfa81
Edmund Li <edmund.xz.lee@gmail.com>
2015-11-08 13:40:34 -0800

    debug

--- app/controllers/api/authorizations_controller.rb
+++ app/controllers/api/authorizations_controller.rb
@@ -67,7 +67,8 @@ class Api::AuthorizationsController < ApplicationController
   	end
 
   	if !auth.uber_registered?
-  		render text: "#{api_activate_url}?user_id=#{auth.slack_user_id}"
+  		render text: params[:user_id]
+  		# render text: "#{api_activate_url}?user_id=#{auth.slack_user_id}"
   	end
   end
 end
