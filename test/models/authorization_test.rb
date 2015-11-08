504e9456d331689bd165d5a0828bf1e415e903f0
Edmund Li <edmund.xz.lee@gmail.com>
2015-11-07 18:13:35 -0800

    added update method

--- test/models/authorization_test.rb
+++ test/models/authorization_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: authorizations
+#
+#  id                  :integer          not null, primary key
+#  slack_user_id       :integer
+#  slack_auth_token    :string
+#  oauth_session_token :string
+#  uber_user_id        :integer
+#  uber_auth_token     :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
 require 'test_helper'
 
 class AuthorizationTest < ActiveSupport::TestCase
