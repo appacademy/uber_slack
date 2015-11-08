2d0501dd37142baa532c014df3a280f34bfd11e1
Matthew Symonds <matthew.symonds@berkeley.edu>
2015-11-07 18:11:10 -0800

    basic #authoriza logic

--- app/controllers/api/authorizations_controller.rb
+++ app/controllers/api/authorizations_controller.rb
@@ -4,4 +4,16 @@ class Api::AuthorizationsController < ApplicationController
   end
 
   def authorize
+    #assumes we have the following: "slack_user_id", "slack_tocken" (session), ""
+    uri = "https://slack.com/oauth/authorize"
+
+    authorize = Authorization.find(user_id: params[:user_id])
+    if authorize
+    else
+      authorize = Authorization.create(user_id: params[:user_id])
+    end
+
+    redirect_to uri
+    
+
   end
