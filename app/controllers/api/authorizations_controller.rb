7748b1d695303e6ca1de669770660d261cf00fcc
Matthew Symonds <matthew.symonds@berkeley.edu>
2015-11-07 18:32:26 -0800

    add query string

--- app/controllers/api/authorizations_controller.rb
+++ app/controllers/api/authorizations_controller.rb
@@ -1,10 +1,12 @@
 class Api::AuthorizationsController < ApplicationController
+  require 'net/http'
   def echo
     render json: params
   end
 
   def authorize
     #assumes we have the following: "slack_user_id", "slack_tocken" (session), ""
+    CLIENT_ID = "aslkjdjasdlkjasdAAAAA"
     uri = "https://slack.com/oauth/authorize"
 
     authorize = Authorization.find(user_id: params[:user_id])
@@ -13,7 +15,6 @@ class Api::AuthorizationsController < ApplicationController
       authorize = Authorization.create(user_id: params[:user_id])
     end
 
-    redirect_to uri
-    
-
+    redirect_to uri + "?client_id=" + CLIENT_ID + "&scope=" + SCOPE +
+    "&redirect_uri=" + base_url + "/api/authorizations/create"
   end
