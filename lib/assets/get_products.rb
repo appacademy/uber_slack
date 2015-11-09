da4975874503b71255f2218f765409a1e82acd71
Andrew Liu <andrew.k.liu89@gmail.com>
2015-11-08 19:05:12 -0800

    Add ride command

--- lib/assets/get_products.rb
+++ lib/assets/get_products.rb
@@ -1,6 +1,6 @@
 require 'addressable/uri'
 
-BASE_URL = "https://sandbox-api.uber.com/v1"
+BASE_URL = "https://sandbox-api.uber.com"
 VALID_COMMANDS = ['ride', 'products']
 
 class UberCommand
@@ -24,6 +24,33 @@ class UberCommand
 
   attr_reader :bearer_token
 
+  def ride input_str
+    origin_name, destination_name = input_str.split("to")
+
+    origin_lat, origin_lng = resolve_address origin_name
+    destination_lat, destination_lng = resolve_address destination_name
+
+    available_products = get_products_for_lat_lng(origin_lat, origin_lng)
+    product_id = available_products["products"].first["product_id"]
+
+    body = {
+      "start_latitude" => origin_lat,
+      "start_longitude" => origin_lng,
+      "end_latitude" => destination_lat,
+      "end_longitude" => destination_lng,
+      "product_id" => product_id
+    }
+
+    response = RestClient.post(
+      "#{BASE_URL}/v1/requests",
+      body.to_json,
+      authorization: bearer_header,
+      "Content-Type" => :json,
+      accept: 'json'
+    )
+
+    return response.body
+  end
 
   def products address
     geocoder_location = Geocoder.search(address)[0].data["geometry"]["location"]
@@ -32,14 +59,13 @@ class UberCommand
   end
 
   def get_products_for_lat_lng lat, lng
-    uri = Addressable::URI.parse("#{BASE_URL}/products")
+    uri = Addressable::URI.parse("#{BASE_URL}/v1/products")
     uri.query_values = { 'latitude' => lat, 'longitude' => lng }
     resource = uri.to_s
 
     result = RestClient.get(
       resource,
-      headers: auth_header,
-      authorization: bearer_string,
+      authorization: bearer_header,
       "Content-Type" => :json,
       accept: 'json'
     )
@@ -47,11 +73,17 @@ class UberCommand
     JSON.parse(result.body)
   end
 
-  def bearer_string
+  def bearer_header
     "Bearer #{bearer_token}"
   end
 
   def invalid_command? name
     VALID_COMMANDS.includes? name ? false : true
   end
+
+  def resolve_address address
+    location = Geocoder.search(address)[0].data["geometry"]["location"]
+    [location['lat'], location['lng']]
+  end
+
 end
