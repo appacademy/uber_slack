148fa79834efde42f3fcb0d2bcf2461f8b80804f
Edmund Li <edmund.xz.lee@gmail.com>
2015-11-08 17:25:23 -0800

    resolved conflict

--- db/migrate/20151109005444_cleandatabase.rb
+++
@@ -1,7 +0,0 @@
-class Cleandatabase < ActiveRecord::Migration
-  def change
-    remove_column :authorizations, :oauth_session_token, :string
-    remove_column :authorizations, :uber_user_id, :string
-    remove_column :authorizations, :webhook, :string
-  end
-end
--- db/migrate/20151109010804_remove_unneeded_columns.rb
+++ db/migrate/20151109010804_remove_unneeded_columns.rb
@@ -3,5 +3,6 @@ class RemoveUnneededColumns < ActiveRecord::Migration
     remove_column :authorizations, :uber_user_id, :integer
     remove_column :authorizations, :oauth_session_token, :string
     remove_column :authorizations, :slack_auth_token, :string
+    remove_column :authorizations, :webhook, :string
   end
 end
