761a8b9c5c92681f86701c43564775d9720e8a2c
Edmund Li <edmund.xz.lee@gmail.com>
2016-04-09 15:27:00 -0700

    moved rollbar log

--- app/controllers/api/authorizations_controller.rb
+++ app/controllers/api/authorizations_controller.rb
@@ -13,12 +13,12 @@ class Api::AuthorizationsController < ApplicationController
 
   # Action for user commands.
   def use_uber
+    Rollbar.info("Request from #{Rails.env")
     auth = Authorization.find_by(slack_user_id: params[:user_id])
     response_url = slack_params[:response_url]
     uber_command = UberCommand.new(auth.uber_auth_token, auth.id, response_url)
     resp = uber_command.run(slack_params[:text])
 
-    Rollbar.info("Request from #{Rails.env}", command: uber_command)
 
     render json: resp
   end
