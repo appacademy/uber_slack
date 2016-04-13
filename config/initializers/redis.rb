016e7e0e8ec935a2e9ff446bd61e76030a1f097c
Edmund Li <edmund.xz.lee@gmail.com>
2016-04-12 18:36:25 -0700

    cached redis instance

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
 uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+REDIS ||= Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
