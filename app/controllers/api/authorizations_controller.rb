b2b553f953d427afdbef691d587cd144ab02d133
Edmund Li <edmund.xz.lee@gmail.com>
2016-04-16 11:51:55 -0700

    fixing connect uber'

--- app/controllers/api/authorizations_controller.rb
+++ app/controllers/api/authorizations_controller.rb
@@ -46,8 +46,7 @@ class Api::AuthorizationsController < ApplicationController
 
   def connect_uber
     # After user has clicked "yes" on Uber OAuth page
-    ConnectUberJob.perform_later(params[:code])
-    render json: { text: "Waiting for Uber oauth." }
+    UberAPI.connect_uber(params[:code])
   end
 
   def establish_session
