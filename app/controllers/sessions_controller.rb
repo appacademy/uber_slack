2832088ba22f4fd2120623426bcf0c2d708a9376
Edmund Li <edmund.xz.lee@gmail.com>
2016-07-23 16:00:24 -0700

    session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class SessionsControllerController < ApplicationController
+class SessionsController < ApplicationController
   def create
     user = User.find_by_credentials(
       params[:user][:username],
