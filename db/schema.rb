540a332554912e124587f5d46a13009f30c3bf88
Edmund Li <edmund.xz.lee@gmail.com>
2016-07-23 14:08:51 -0700

    added password column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413012532) do
+ActiveRecord::Schema.define(version: 20160723210742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20160413012532) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "password"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
