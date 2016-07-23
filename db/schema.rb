0732cce94acb6caf7eb2f99532b74d478091d8a7
Edmund Li <edmund.xz.lee@gmail.com>
2016-07-23 14:13:36 -0700

    rename password column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723210919) do
+ActiveRecord::Schema.define(version: 20160723211211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,13 +46,13 @@ ActiveRecord::Schema.define(version: 20160723210919) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                      null: false
+    t.string   "email",                           null: false
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.string   "password"
-    t.boolean  "admin",      default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
