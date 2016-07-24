4304f6c783c155cc4fc5a4e8a999c2cd8bc9ba0c
Edmund Li <edmund.xz.lee@gmail.com>
2016-07-23 19:14:34 -0700

    limit access to sidekiq dash

--- lib/admin_constraint.rb
+++ lib/admin_constraint.rb
@@ -1,6 +1,5 @@
 class AdminConstraint
   def matches?(request)
-    return true
     session_token = request.path_parameters[:token]
     return false unless session_token
     user = User.find_by(session_token: session_token)
